739f3b7a73956e209d65008b959988aabfc38c9d
lolochka132 <prostololi@mail.ru>
2025-05-16 21:42:31 +0300

    Initial commit

---
+++ devops-lab5-main/devops-lab5-main/tests/test_user.py
@@ -0,0 +1,50 @@
+from fastapi.testclient import TestClient
+from src.main import app
+
+client = TestClient(app)
+
+users = [
+    {
+        'id': 1,
+        'name': 'Ivan Ivanov',
+        'email': 'i.i.ivanov@mail.com',
+    },
+    {
+        'id': 2,
+        'name': 'Petr Petrov',
+        'email': 'p.p.petrov@mail.com',
+    }
+]
+
+def test_get_existed_user():
+    '''Получение существующего пользователя'''
+    response = client.get("/api/v1/user", params={'email': users[0]['email']})
+    assert response.status_code == 200
+    assert response.json() == users[0]
+
+def test_get_unexisted_user():
+    '''Получение несуществующего пользователя'''
+    response = client.get("/api/v1/user", params={'email': "nonexistent@mail.com"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "User not found"}
+
+def test_create_user_with_valid_email():
+    '''Создание пользователя с уникальной почтой'''
+    new_user = {
+        "name": "New User",
+        "email": "new.user@mail.com"
+    }
+    response = client.post("/api/v1/user", json=new_user)
+    assert response.status_code == 201
+    assert isinstance(response.json(), int)  
+
+def test_create_user_with_invalid_email():
+    '''Создание пользователя с занятой почтой'''
+    response = client.post("/api/v1/user", json={"name": "Ivan Ivanov", "email": users[0]['email']})
+    assert response.status_code == 409
+    assert response.json() == {"detail": "User with this email already exists"}
+
+def test_delete_user():
+    '''Удаление пользователя'''
+    response = client.delete("/api/v1/user", params={'email': users[0]['email']})
+    assert response.status_code == 204
